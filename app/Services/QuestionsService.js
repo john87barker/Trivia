ee71c940f86f77ebdcb54b69bf8adbc5b6bad918
john87barker <john87barker@gmail.com>
2021-07-19 15:55:48 -0600

    service

--- app/Services/QuestionsService.js
+++ app/Services/QuestionsService.js
@@ -1,10 +1,18 @@
 import { ProxyState } from "../AppState.js";
 import Question from "../Models/Question.js";
+import { api } from "./AxiosService.js"
 
 class QuestionsService {
-  addValue() {
-    ProxyState.questions = [...ProxyState.questions, new Question({ title: Math.random() })]
+  async getQuestion(url) {
+    const res = await api.get(url)
+    console.log(res.data.results)
+    ProxyState.next = res.data.next
+    ProxyState.previous = res.data.previous
+
+    let question = res.data.results.map(q => new Question(q))
+    ProxyState.question = question
   }
+
 }
 
 export const questionsService = new QuestionsService();
